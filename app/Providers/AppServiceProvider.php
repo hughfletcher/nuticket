a20b9811cd3b839fffc9fbdbdefd15d29595e912
Hugh Fletcher <hugh@hughfletcher.com>
2015-05-30 08:23:46 -0500

    Added support for workbench and dynamically adding workbench packages.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Workbench\Starter;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -21,10 +22,30 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
+		$this->develop();
+		
+	}
 
-		if (!$this->app->environment('production')) {
-			$this->app->register('Clockwork\Support\Laravel\ClockworkServiceProvider');
+	public function develop() 
+	{
+		if ($this->app->environment('production')) 
+		{
+			return;
 		}
+
+		$this->app->register('Clockwork\Support\Laravel\ClockworkServiceProvider');
+		$this->app->register('Illuminate\Workbench\WorkbenchServiceProvider');
+
+		if (is_dir($workbench = base_path() . '/workbench'))
+		{
+		    Starter::start($workbench);
+		}
+
+		if (is_file(base_path() . '/.workbench.php'))
+		{
+			include(base_path() . '/.workbench.php');
+		}
+
 	}
 
 }
