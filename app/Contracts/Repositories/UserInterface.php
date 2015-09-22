400c4cb0a9915c727e9216d7bfa8b6ba45de639b
Hugh Fletcher <hughafletcher@gmail.com>
2015-09-22 10:45:53 -0500

    Move user repo and add event to add users from other sources.

---
+++ app/Contracts/Repositories/UserInterface.php
@@ -0,0 +1,14 @@
+<?php namespace App\Contracts\Repositories;
+
+interface UserInterface {
+
+	// public function lists($value, $key = 'id');
+
+	// public function allWithTrashed();
+
+	// public function all();
+
+	// public function destroy($id);
+
+	// public function insert($data);
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,84 @@
+<?php namespace App\Repositories;
+
+use App\Contracts\Repositories\UserInterface;
+use Bosnadev\Repositories\Eloquent\Repository;
+use Bosnadev\Repositories\Contracts\RepositoryInterface;
+use App\Events\FetchedUsersEvent;
+
+class UserRepository extends Repository implements UserInterface, RepositoryInterface {
+
+	public function model() {
+        return 'App\User';
+    }
+
+    public function all($columns = ['*'], array $params = array(), $defer_listeners = false) 
+    {
+    	$this->pushCriteria(new Criteria\RequestSearchUsers($params));
+    	$results = parent::all($columns);
+    	$params['defer_listeners'] = $defer_listeners;
+
+    	$event_data = event(new FetchedUsersEvent($results, $params));
+
+    	foreach ($event_data as $data) {
+
+    		foreach ($data as $row) {
+    			$results->put($results->count(), $row);
+    		}
+    		
+    	}
+    	return $results;
+    }
+
+	// public function lists($value, $key = 'id') {
+	// 	return $this->user->lists($value, $key);
+	// }
+
+	// public function allWithTrashed() {
+	// 	return $this->user->withTrashed()->get()->toArray();
+	// }
+
+
+	// public function get() {
+	// 	return $this->model->get()->toArray();
+	// }
+
+	// public function destroy($id) {
+	// 	$this->user->destroy($id);
+	// }
+
+	// public function insert($data) {
+	// 	$row = $this->user->create($data);
+	// 	return $row->id;
+	// }
+
+	// public function fields($cols) {
+
+	// 	if (empty($cols)) {
+	// 		return $this;
+	// 	}
+
+	// 	$this->model = $this->model->select($cols);
+
+	// 	return $this;
+	// }
+
+	/**
+	 * Search filter on query
+	 * 
+	 * @param  array $query
+	 * @param  array $cols
+	 * @return $this
+	 */
+	// public function whereSearch(array $query = [], array $cols = []) {	
+
+	// 	$search = ['display_name'];
+
+	// 	return parent::whereSearch($query, $search);
+	// }
+
+	// public function findBy($attribute, $value, $columns = array('*')) {
+
+ //        return $this->model->where($attribute, '=', $value)->first($columns);
+ //    }
+
+}
