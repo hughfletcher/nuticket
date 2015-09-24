0b72a96693b86eef9d396a12868d4118b10205ea
Hugh Fletcher <hughafletcher@gmail.com>
2015-09-23 20:08:20 -0500

    Renamed so more consistent.

--- app/Events/UsersGetAllEvent.php
+++ app/Events/UsersGetAllEvent.php
@@ -6,7 +6,7 @@ use App\Events\Event;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class FetchedUsersEvent extends Event
+class UsersGetAllEvent extends Event
 {
     use SerializesModels;
 
