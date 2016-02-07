e0256da6d366e04cf7e64f35c4e2c61745539ae7
Hugh Fletcher <hughafletcher@gmail.com>
2016-02-07 12:20:03 -0600

    Consolidated Reports and Piper into App provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Workbench\Starter;
+use App\Services\Notify\SendMessage;
+use App\Services\Piper\Manager as PiperManager;
+use App\Services\Reports\Manager as ReportsManager;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -22,7 +25,30 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-	
+		// $this->registerNotify();
+		$this->registerPiper();
+		$this->registerReports();
 	}
+
+	public function registerPiper()
+	{
+		$this->app->bind('App\Services\Piper\Manager', function ($app) {
+            return new PiperManager($app);
+        });
+	}
+
+	public function registerNotify()
+	{
+		$this->app['notifynder']->extend('sendMessage', function($notifications,$app) {
+            return new SendMessage($notifications);
+        });
+	}
+
+	public function registerReports()
+    {
+        $this->app->singleton('App\Services\Reports\Manager', function ($app) {
+            return new ReportsManager($app);
+        });
+    }
 
 }
