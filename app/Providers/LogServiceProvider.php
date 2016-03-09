214364d091054fb6253cca3332e0a5f6886232bd
Hugh Fletcher <hughafletcher@gmail.com>
2016-03-09 16:28:54 -0600

    Added emailing errors to admin.

--- app/Providers/LogServiceProvider.php
+++ app/Providers/LogServiceProvider.php
@@ -2,22 +2,32 @@
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\ConfigInterface;
+use Monolog\Handler\SwiftMailerHandler;
+use Swift_Message, Logger;
 
 class LogServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-
-
     public function boot()
     {
         $monolog = $this->app['log']->getMonolog();
+
+        $user = $this->app->make('App\Contracts\Repositories\UserInterface');
+        $admins = $user->findAllBy('is_admin', true)->lists('display_name', 'email');
+
         foreach($monolog->getHandlers() as $handler) {
             $handler->setLevel($this->app['config']->get('settings.log.level'));
+        }
+
+        if ($this->app->getEnviroment() == 'production' && config('notify.system.admin')) {
+            $monolog->pushHandler(
+                new SwiftMailerHandler(
+                    $this->app['mailer']->getSwiftMailer(),
+                    Swift_Message::newInstance('[Log] An Error Occured!')->setFrom(config('settings.mail.admin'))->setTo($admins->toArray()),
+                    'error', // set minimal log lvl for mail
+                    true // bubble to next handler?
+                )
+            );
         }
     }
 
